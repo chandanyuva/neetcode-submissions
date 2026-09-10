b49c1d80942f21691cc5a98ceb91d066eb0b63cd
Chandan K P <chandanyuva1511@gmail.com>
2026-09-10 15:29:36 +0530

    Add: longest-consecutive-sequence - submission-3

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-3.js
@@ -0,0 +1,26 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    longestConsecutive(nums) {
+        if (nums.length==0)return 0
+        nums.sort((a, b) => a - b);
+        let count = 1;
+        let max = count;
+        for (let i = 0; i < nums.length; i++) {
+            count = 1;
+            for (let j = i + 1; j < nums.length; j++) {
+                // console.log(`${i}:${j} \t${nums[i]} : ${nums[j]}\t ${nums[j]-nums[i]}\t ${count}\t ${max}`)
+                if (nums[j] - nums[i] == 1) {
+                    count++;
+                    i = j;
+                } else {
+                    continue;
+                }
+                if (count > max) max = count;
+            }
+        }
+        return max;
+    }
+}
