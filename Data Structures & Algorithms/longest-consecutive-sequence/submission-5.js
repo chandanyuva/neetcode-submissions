7e2909dab1ad5a8dbdccc573f9daf5624b840151
Chandan K P <chandanyuva1511@gmail.com>
2026-09-11 09:51:07 +0530

    Add: longest-consecutive-sequence - submission-5

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-5.js
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    longestConsecutive(nums) {
+        let numsSet = new Set(nums);
+        let res=0;
+        for (let i=0 ; i<nums.length;i++){
+            let streak = 0;
+            let curr = nums[i];
+            while (numsSet.has(curr)){
+                streak++
+                curr++
+            }
+            res = Math.max(res,streak)
+        } 
+        return res
+    }
+}
